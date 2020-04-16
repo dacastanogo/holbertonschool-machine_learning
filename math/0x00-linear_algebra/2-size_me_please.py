e973e2c00a63c9d473adfa6ce9dc01662b4ac357
dacastanogo <dcastanogomez@gmail.com>
2020-04-16 15:55:46 -0500

    fixed output

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -2,12 +2,19 @@
 """
 Calculates the shape of a matrix
 """
+
+
 def matrix_shape(matrix):
+    """
+    Getting shape of a matrix
+    Return array with dimensions
+    """
     if not matrix:
         return None
     shape = []
     shape.append(len(matrix))
     shape.append(len(matrix[0]))
-    if (shape.append(len(matrix[0][0]))):
+    print ( "len after 2 dimensions is: {}".format(len(matrix[0])))
+    if (len(matrix[0])) > 2:
         shape.append(len(matrix[0][0]))
     return(shape)
