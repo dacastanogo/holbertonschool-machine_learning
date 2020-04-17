c5c47334fe162d67629e1b52fc1cac041c387656
dacastanogo <dcastanogomez@gmail.com>
2020-04-16 20:51:55 -0500

    Added task 13 and 14

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+function that performs matrix multiplication
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    function that performs matrix multiplication
+    """
+    return np.matmul(mat1, mat2)
