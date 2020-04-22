fc9b311f003babe229665a93ef333c5154c2a670
dacastanogo <dcastanogomez@gmail.com>
2020-04-21 22:11:20 -0500

    Corrected the whole barn task

--- math/0x00-linear_algebra/101-the_whole_barn.py
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -1,18 +1,42 @@
 #!/usr/bin/env python3
+"""Adds two matrices
 """
-function adds two matrices
-"""
+
+
+def shape(matrix):
+    """
+    Getting shape of a matrix
+    Return array with dimensions
+    """
+    shape = [len(matrix)]
+    while type(matrix[0]) == list:
+        shape.append(len(matrix[0]))
+        matrix = matrix[0]
+    return shape
+
+
+def shape_recursion(mat1, mat2):
+    """
+    Getting shape of a matrix
+    Return array with dimensions
+    """
+    add = []
+    for i in range(len(mat1)):
+        if type(mat1[i]) == list:
+            add.append(shape_recursion(mat1[i], mat2[i]))
+        else:
+            add.append(mat1[i] + mat2[i])
+    return add
+
 
 def add_matrices(mat1, mat2):
-
     """
-    adds two matrices
-    return added matrix
+    Adds two matrices
     """
-    if len(mat1[0]) != len(mat2[0]):
-        return (None)
-    newMat = [[] for i in mat1]
-    for i in range(len(mat1)):
-        for j in range(len(mat1[0])):
-            newMat[i].append(mat1[i][j] + mat2[i][j])
-    return (newMat)
+    shape1 = shape(mat1)
+    shape2 = shape(mat2)
+    tempshape = shape1
+    if shape1 != shape2:
+        return None
+    add = shape_recursion(mat1, mat2)
+    return add
