0291e14f4710084c47268a91ba33a7cd9d5936fb
dacastanogo <dcastanogomez@gmail.com>
2020-04-26 21:33:17 -0500

    Added 17 task

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Calculates the integral of a polynomial
+"""
+
+
+def poly_integral(poly, C=0):
+    """
+        calculates the integral of a polynomial
+        Return: list of integers, or None if poly or C is invalid
+    """
+    if not type(poly) is list or len(poly) == 0 or\
+            not (type(poly[0]) is int or type(poly[0] is float)):
+        return None
+    if not (type(C) is int or type(C) is float):
+        return None
+
+    integral = [C]
+    for i, coeff in enumerate(poly, 1):
+        if coeff % i == 0:
+            integral.append(coeff // i)
+        else:
+            integral.append(coeff / i)
+    for i in range(len(integral) - 1, 0, -1):
+        if integral[i] == 0:
+            integral.pop(i)
+        else:
+            break
+    return integral
