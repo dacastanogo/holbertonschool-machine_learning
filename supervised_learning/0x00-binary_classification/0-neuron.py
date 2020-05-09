8dcc1b7b3eb2c4c3d55d8ada01fc942045f0f326
dacastanogo <dcastanogomez@gmail.com>
2020-05-08 20:19:41 -0500

    Modified nx

--- supervised_learning/0x00-binary_classification/0-neuron.py
+++ supervised_learning/0x00-binary_classification/0-neuron.py
@@ -15,7 +15,6 @@ class Neuron:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        self.nx = nx
         self.W = np.ndarray((1, nx))
         self.W[0] = np.random.normal(size=nx)
         self.b = 0
