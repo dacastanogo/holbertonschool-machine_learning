COrrected gradient descent

supervised_learning/0x00-binary_classification/15-neural_network.py
@@ -96,14 +96,13 @@ class NeuralNetwork:
                 self.cost(Y, self.__A2))
 
     def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
-        """
-        calculates one step gradient descent on the Neural Network
-        """
-        dpW2 = np.dot(self.__W2.T, (A2 - Y)) * A1 * (1 - A1)
-        self.__W1 = self.__W1 - alpha * np.dot(dpW2, X.T) / X.shape[1]
-        self.__W2 = self.__W2 - alpha * np.dot((A2 - Y), A1.T) / A1.shape[1]
-        self.__b1 = self.__b1 - alpha * (A2 - Y).mean(axis=1, keepdims=True)
-        self.__b2 = self.__b2 - alpha * dpW2.mean(axis=1, keepdims=True)
+        """calculates one pass gradient descent on the NN"""
+        dz2 = A2 - Y
+        dz1 = np.matmul(self.__W2.T, dz2) * A1 * (1 - A1)
+        self.__W1 = self.__W1 - alpha * np.matmul(dz1, X.T) / X.shape[1]
+        self.__W2 = self.__W2 - alpha * np.matmul(dz2, A1.T) / A1.shape[1]
+        self.__b1 = self.__b1 - alpha * dz1.mean(axis=1, keepdims=True)
+        self.__b2 = self.__b2 - alpha * dz2.mean(axis=1, keepdims=True)
 
     def train(self, X, Y, iterations=5000, alpha=0.05,
               verbose=True, graph=True, step=100):