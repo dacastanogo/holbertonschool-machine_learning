6bbedaacdf3d10c4b6a6c08bbf97f01a16bf56fb
Daniel C <51679787+dacastanogo@users.noreply.github.com>
2020-07-28 10:12:52 -0500

    Corrected typo

--- math/0x01-plotting/4-frequency.py
+++ math/0x01-plotting/4-frequency.py
@@ -9,7 +9,7 @@ plt.hist(student_grades, facecolor='b', edgecolor='black', bins=np.arange(0, 110
 plt.xticks(np.arange(0, 110, 10))
 plt.xlim(0, 100)
 plt.yticks(np.arange(0, 35, 5))
-plt.xlabel('Grandes')
+plt.xlabel('Grades')
 plt.ylabel('Number of Students')
 plt.title('Project A')
 plt.show()
