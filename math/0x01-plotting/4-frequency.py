e977e7f91f46902dd0da964767ff3b73600739e9
Daniel C <51679787+dacastanogo@users.noreply.github.com>
2020-07-28 10:11:53 -0500

    Update 4-frequency.py
    Optimized code

--- math/0x01-plotting/4-frequency.py
+++ math/0x01-plotting/4-frequency.py
@@ -5,8 +5,7 @@ import matplotlib.pyplot as plt
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
 
-xvals = np.arange(0, 110, 10)
-plt.hist(student_grades, facecolor='b', edgecolor='black', bins=xvals)
+plt.hist(student_grades, facecolor='b', edgecolor='black', bins=np.arange(0, 110, 10))
 plt.xticks(np.arange(0, 110, 10))
 plt.xlim(0, 100)
 plt.yticks(np.arange(0, 35, 5))
