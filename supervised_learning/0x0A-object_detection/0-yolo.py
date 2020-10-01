f512f9349ee9a98d403dbab026aac4cd34a591fb
dacastanogo <dcastanogomez@gmail.com>
2020-10-01 12:26:43 -0500

    Dir created and first task completed ab

--- supervised_learning/0x0A-object_detection/0-yolo.py
+++ supervised_learning/0x0A-object_detection/0-yolo.py
@@ -3,13 +3,13 @@
 import tensorflow.keras as K
 
 
-class Yolo:
-    """ Yolo v3 algorithm to perform object detection"""
+class Yolo():
+    """Class constructor"""
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """ init """
         self.model = K.models.load_model(model_path)
-        with open(classes_path, 'r') as f:
-            self.class_names = [line.strip() for line in f]
+        with open(classes_path, 'r') as classFile:
+            self.class_names = [line.strip() for line in classFile]
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
