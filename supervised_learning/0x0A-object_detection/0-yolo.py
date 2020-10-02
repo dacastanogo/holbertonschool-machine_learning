72941609ec72c152edfb8138c9ff2da9df586ecd
dacastanogo <dcastanogomez@gmail.com>
2020-10-02 18:13:36 -0500

    removed readme typo, changed 0 task

--- supervised_learning/0x0A-object_detection/0-yolo.py
+++ supervised_learning/0x0A-object_detection/0-yolo.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-""" Initialize Yolo v3 """
+""" Initialize Yolo """
 import tensorflow.keras as K
 
 
-class Yolo():
-    """Class constructor"""
+class Yolo:
+    """ YOYO V3"""
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """ init """
         self.model = K.models.load_model(model_path)
-        with open(classes_path, 'r') as classFile:
-            self.class_names = [line.strip() for line in classFile]
+        with open(classes_path, 'r') as f:
+            self.class_names = [line.strip() for line in f]
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
